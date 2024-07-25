2f2e1018f37a438b4403f0d986ded11ad97cfb2b
Samriddha11 <samriddhachoudhuri@gmail.com>
2024-07-25 14:38:46 +0530

    update

--- main.tf
+++ main.tf
@@ -1,110 +1,35 @@
 provider "aws" {
-  region = "us-west-2" # Adjust the region as needed
+  region = "us-east-1" # Adjust the region as needed
 }
-
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "eks-vpc"
-  }
-}
-
-resource "aws_subnet" "eks_subnet" {
-  count             = 3
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
-
-  tags = {
-    Name = "eks-subnet-${count.index}"
-    "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-}
-
-data "aws_availability_zones" "available" {}
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  role_arn = "arn:aws:iam::759984737373:role/ccm-sam-eks"
 
   vpc_config {
-    subnet_ids = aws_subnet.eks_subnet[*].id
+    subnet_ids = [
+      "subnet-075ac06f814233f40",
+      "subnet-089e7a40da26dd845",
+      "subnet-0d3650898e5d4ca9a",
+      "subnet-03f25ce1cd0535df9",
+    ]
   }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy]
-}
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-resource "aws_iam_role" "eks_node_group_role" {
-  name = "eks-node-group-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_group_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_group_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_group_role.name
 }
 
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = aws_subnet.eks_subnet[*].id
+  node_role_arn   = "arn:aws:iam::759984737373:role/AmazonEKSNodeRoleCCM"
+  subnet_ids      = [
+    "subnet-075ac06f814233f40",
+    "subnet-089e7a40da26dd845",
+    "subnet-0d3650898e5d4ca9a",
+    "subnet-03f25ce1cd0535df9",
+  ]
 
   scaling_config {
     desired_size = 2
     max_size     = 3
     min_size     = 1
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_node_group_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.eks_node_group_AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.eks_node_group_AmazonEKS_CNI_Policy,
-  ]
 }
